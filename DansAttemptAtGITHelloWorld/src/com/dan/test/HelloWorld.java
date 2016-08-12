f16bf454b9fa126b0b776c80f0429743ec904390
dhiebert <dhiebert@optimus.rchland.ibm.com>
2016-08-12 14:02:24 -0600

    changed stuff

---
+++ DansAttemptAtGITHelloWorld/src/com/dan/test/HelloWorld.java
@@ -0,0 +1,55 @@
+package com.dan.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 5L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		System.out.println("Hello World Start");
+		String name=request.getParameter("Name");		
+		PrintWriter out = response.getWriter();		
+		out.println("<html><head><title>HelloWorld</title></head>");
+		out.println("<body>");
+        out.println("<br>HelloWorld<br>");
+		out.println("<p>");
+		out.println("Dans Hello World = "+ name);
+		out.println("</body></html>");						
+		out.flush();	
+		out.close();
+		System.out.println("Hello World Done");
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
